069572d208e28973a0320cfa24eee5981024e741
AkhilChandran <akhil@trycle.com>
2026-09-10 11:20:37 +0530

    deployement backend update 3-cors

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.app = void 0;
 const express_1 = __importDefault(require("express"));
@@ -9,10 +11,15 @@ const routes_1 = __importDefault(require("./routes"));
 const cors_1 = __importDefault(require("cors"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 exports.app = (0, express_1.default)();
-exports.app.use((0, cors_1.default)({
-    origin: "http://localhost:5173",
+exports.app.use(
+  (0, cors_1.default)({
+    origin: [
+      "http://localhost:5173",
+      "https://aitreasurer-frontend.onrender.com",
+    ],
     credentials: true,
-}));
+  }),
+);
 exports.app.use(express_1.default.json());
 exports.app.use((0, cookie_parser_1.default)());
 exports.app.use("/api", routes_1.default);
--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,10 @@ export const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://aitreasurer-frontend.onrender.com",
+    ],
     credentials: true,
   }),
 );
